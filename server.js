b3cfa54908660b4982bc5d7acff8b4bf2ca9a941
Nikhithasriram <nikhithasriram.552@gmail.com>
2025-01-09 22:59:36 +0530

    setting up env

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("hey")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get('/api/create-room' , (req,res)=>{
+    res.status(200).send("Hello");
+})
+
+app.listen(port , ()=>{
+    console.log("Server running on port 5000");
+})
+
+console.log("hey123")
