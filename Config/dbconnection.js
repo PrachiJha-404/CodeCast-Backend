a0184824fc4bfff7f8da00f4ffe453c2e255d159
Nikhithasriram <nikhithasriram.552@gmail.com>
2025-01-11 00:09:53 +0530

    express app

--- Config/dbconnection.js
+++ Config/dbconnection.js
@@ -3,7 +3,7 @@ const constants = require("../constants.js")
 async function DBConnection  (){
     try {
         let connection = await mongoose.connect(`${process.env.CONNECTION_STRING}/${constants.DB_NAME}`)
-        console.log("Database connected sucessfully" , connection)
+        console.log("Database connected sucessfully")
     } catch (error) {
         console.log("MONGODB connection error" , error);
         throw error
--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const app = express();
 const DBConnection = require("./Config/dbconnection.js")
 const port = process.env.PORT || 8000;
 
-DBConnection();
+const app = express();
+
+DBConnection()
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log("Server running on port",port)
+    })
+    app.on("error", (err)=>{
+        console.log("Error" , err)
+        throw err
+    })
+})
+.catch((err)=>{
+    console.log("Database Connection failed" , err)
+});
+
+
 app.get('/api/create-room' , (req,res)=>{
     res.status(200).send("Hello");
 })
 
-app.listen(port , ()=>{
-    console.log("Server running on port 5000");
-})
-
-console.log("hey123")
