98473609a3d6c77d0bb123395e7721bc13793630
PrachiJha-404 <prachijha1805@gmail.com>
2025-01-30 12:39:32 +0530

    making nikhitha's life easier

--- models/userModel.js
+++ models/userModel.js
@@ -13,11 +13,6 @@ const userSchema = new mongoose.Schema(
             required: true
         },
 
-        username: {
-            type: String,
-            required: true
-        },
-
         email: {
             type: String,
             required: true,
--- server.js
+++ server.js
@@ -8,28 +8,28 @@ dotenv.config()
 const app = express();
 app.use(cors())
 app.use(cookieParser())
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true , limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 DBConnection()
-.then(()=>{
-    app.listen(port , ()=>{
-        console.log("Server running on port",port)
+    .then(() => {
+        app.listen(port, () => {
+            console.log("Server running on port", port)
+        })
+        app.on("error", (err) => {
+            console.log("Error", err)
+            throw err
+        })
     })
-    app.on("error", (err)=>{
-        console.log("Error" , err)
-        throw err
-    })
-})
-.catch((err)=>{
-    console.log("Database Connection failed" , err)
-});
+    .catch((err) => {
+        console.log("Database Connection failed", err)
+    });
 
 //routes
 
 import userRouter from "./routes/user.routes.js";
 
-app.use("/users" , userRouter)
+app.use("/users", userRouter)
 
 
 
